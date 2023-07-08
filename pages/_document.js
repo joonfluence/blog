9d4ef2fe2f6f50eba87724af0f7edec726e0c551
joonho lee <joonfluence.dev@gmail.com>
2023-07-08 23:30:12 +0900

    [FIX] Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,7 @@ class MyDocument extends Document {
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-          <Script src="./google-analytics.js" />
+          <Script src="../lib/google-analytics.js" />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
           <Main />
--- pages/google-analytics.js
+++
@@ -1,8 +0,0 @@
-import siteMetadata from '@/data/siteMetadata'
-
-window.dataLayer = window.dataLayer || []
-function gtag() {
-  window.dataLayer.push(arguments)
-}
-gtag('js', new Date())
-gtag('config', siteMetadata.googleAnalyticsId)

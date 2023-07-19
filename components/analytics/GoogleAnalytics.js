4437e76c24fc897b781ad88094defca984d2f6ea
joonho lee <joonfluence.dev@gmail.com>
2023-07-19 22:12:50 +0900

    [FIX] Google Analytics (2)

--- components/analytics/GoogleAnalytics.js
+++ components/analytics/GoogleAnalytics.js
@@ -1,17 +1,15 @@
-import Script from 'next/script'
-
 import siteMetadata from '@/data/siteMetadata'
 
 const GAScript = () => {
   return (
     <>
-      <Script
+      <script
         async
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${siteMetadata.analytics.googleAnalyticsId}`}
       />
 
-      <Script strategy="lazyOnload" id="ga-script">
+      <script strategy="lazyOnload" id="ga-script">
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -20,7 +18,7 @@ const GAScript = () => {
               page_path: window.location.pathname,
             });
         `}
-      </Script>
+      </script>
     </>
   )
 }
